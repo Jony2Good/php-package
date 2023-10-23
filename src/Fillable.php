d95d5ea21850b26c6149dac002892b74e25b2d30
artem_emelianenko <akk_12@mail.ru>
2023-10-23 19:27:42 +0300

    add: new tests

--- src/Fillable.php
+++ src/Fillable.php
@@ -4,6 +4,12 @@ namespace Unit;
 
 class Fillable
 {
-
-
+    public function fill(array &$coll, $value, int $start = 0, $end = null): array
+    {
+        $endArr = min(count($coll), is_null($end) ? count($coll) : $end);
+        for ($i = $start; $i < $endArr; $i++) {
+            $coll[$i] = $value;
+        }
+        return $coll;
+    }
 }
---
+++ tests/FillableTest.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace Unit\Tests;
+
+use PHPUnit\Framework\TestCase;
+use Unit\Fillable;
+use Unit\Storage;
+
+class FillableTest extends TestCase
+{
+  private Fillable $object;
+
+    protected function setUp(): void
+    {
+        $this->object = new Fillable();
+    }
+    public function testFillArrayWithSingleValue()
+    {
+        $input = [1, 2, 3, 4, 5];
+        $value = 10;
+        $expected = [10, 10, 10, 10, 10];
+
+        $result = $this->object->fill($input, $value);
+        $this->assertEquals($expected, $result);
+    }
+
+    public function testFillArrayWithSingleValueAndStartIndex()
+    {
+        $input = [1, 2, 3, 4, 5];
+        $value = 10;
+        $start = 2;
+        $expected = [1, 2, 10, 10, 10];
+
+        $result = $this->object->fill($input, $value, $start);
+        $this->assertEquals($expected, $result);
+    }
+
+    public function testFillArrayWithSingleValueAndStartEndIndexes()
+    {
+        $input = [1, 2, 3, 4, 5];
+        $value = 10;
+        $start = 1;
+        $end = 3;
+        $expected = [1, 10, 10, 4, 5];
+
+        $result = $this->object->fill($input, $value, $start, $end);
+        $this->assertEquals($expected, $result);
+    }
+
+    public function testFillArrayWithSingleValueAndEndIndexGreaterThanArraySize()
+    {
+        $input = [1, 2, 3, 4, 5];
+        $value = 10;
+        $end = 8;
+        $expected = [10, 10, 10, 10, 10];
+
+        $result = $this->object->fill($input, $value, 0, $end);
+        $this->assertEquals($expected, $result);
+    }
+
+    public function testFillArrayWithSingleValueAndStartEndIndexesOutOfRange()
+    {
+        $input = [1, 2, 3, 4, 5];
+        $value = 10;
+        $start = 4;
+        $end = 2;
+        $expected = [1, 2, 3, 4, 5];
+
+        $result = $this->object->fill($input, $value, $start, $end);
+        $this->assertEquals($expected, $result);
+    }
+}
